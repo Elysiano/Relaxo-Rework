24428f80e74b7420616594c8ec796c0cf4c075b3
Elysiano <patrick.volleybal@hotmail.com>
2015-08-19 18:08:05 +0200

    Progressbar slide effect / icons added

--- Relaxo Rework Unity/Assets/Scripts/UIManager.cs
+++ Relaxo Rework Unity/Assets/Scripts/UIManager.cs
@@ -3,19 +3,44 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
+	public bool toggleDisplayed = false;
+
+	public void Update()
+	{
+		if (toggleDisplayed) 
+		{
+			//turn arrow
+		}
+	}
+
 	public void DisableBoolInAnimator(Animator anim)
 	{
 		anim.SetBool ("isDisplayed", false);
+		toggleDisplayed = false;
 	}
 
 	public void EnableBoolInAnimator (Animator anim)
 	{
 		anim.SetBool ("isDisplayed", true);
+		toggleDisplayed = true;
 	}
 
 	public void NavigateTo(int scene)
 	{
 		Application.LoadLevel (scene);
+	}
+
+	public void ToggleBoolInAnimator(Animator anim)
+	{
+		if (toggleDisplayed == false) {
+			anim.SetBool ("isDisplayed", true);
+			toggleDisplayed = true;
+		} 
+		else 
+		{
+			anim.SetBool ("isDisplayed", false);
+			toggleDisplayed = false;
+		}
 	}
 
 }
