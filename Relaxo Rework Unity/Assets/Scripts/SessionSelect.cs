853acab94c82e5ea12557a3cab4ad37650b4db01
BryanKlunder <bryanklunder@hotmail.com>
2015-09-03 18:57:37 +0200

    Select Session

--- Relaxo Rework Unity/Assets/Scripts/SessionSelect.cs
+++ Relaxo Rework Unity/Assets/Scripts/SessionSelect.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 public class SessionSelect : MonoBehaviour
 {
 	Color selectedColor = new Color (0.796f, 0.709f, 0.392f, 1f);
-	Color unselectedColor = new Color (0.49f, 0.49f, 0.49f, 1f);
+	Color sessionColor = new Color (0.49f, 0.49f, 0.49f, 1f);
+
+	bool hitSession = false;
+	bool casting = false;
 
 	void Start ()
 	{
@@ -18,18 +21,35 @@ public class SessionSelect : MonoBehaviour
 		RaycastHit hit;
 		Debug.DrawRay (ray.origin, ray.direction * 500, Color.blue);
 
-		if (Physics.Raycast(ray, out hit, 500))
+		if (Physics.Raycast (ray, out hit, 500))
 		{
-			if (hit.collider.tag == "SelectedSession")
+			if (hit.collider.tag == "Session")
 			{
-				GameObject.FindWithTag ("SelectedSession").GetComponent<Image> ().color = selectedColor;
-				print ("Ik raak het actieve shitje");
+				hitSession = true;
+				casting = true;
 			}
 		}
-	}
+		else
+		{
+			hitSession = false;
+		}
 
-	void SelectSession ()
-	{
+		if (hitSession)
+		{
+			hit.collider.gameObject.GetComponent<Image> ().color = selectedColor;
+			hit.collider.transform.localScale = new Vector3 (1f, 1f, 1f);
+			hit.collider.tag = "SelectedSession";
+		}
 
+		if (!hitSession)
+		{
+			if (casting)
+			{
+				GameObject.FindWithTag ("SelectedSession").GetComponent<Image> ().color = sessionColor;
+				GameObject.FindWithTag ("SelectedSession").transform.localScale = new Vector3 (0.8f, 0.8f, 1f);
+				GameObject.FindWithTag ("SelectedSession").tag = "Session";
+				casting = false;
+			}
+		}
 	}
 }
